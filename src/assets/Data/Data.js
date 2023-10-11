40324de986b6c808110165f6f6a27cd60b2a44b7
AngeAnjara <angeloelano2@gmail.com>
2023-10-11 21:38:41 +0300

    logoFix

--- src/assets/Data/Data.js
+++ src/assets/Data/Data.js
@@ -3,28 +3,28 @@ export const Produits = [
         id:"1",
         nomProduit: "Poivre Grena ",
         itemProduit: "Un Piquant d'Excellence",
-        photo: "/src/assets/poivre.jpg",
+        photo: "./src/assets/poivre.jpg",
         desc: "Le poivre grena, cultivé avec soin sur nos terres fertiles, apporte une dimension piquante et aromatique à vos plats. Découvrez une explosion de saveurs provenant   la nature elle-même. Chaque grain de poivre grena de VanillaVoyage est une promesse de qualité, de fraîcheur et de l'authenticité unique de Madagascar."
     },
     {
         id:"2",
         nomProduit: "Huile Essentielle d'Ylang-Ylang ",
         itemProduit: "L'Élégance Florale",
-        photo: "/src/assets/Ylang-ylang.jpeg",
+        photo: "./src/assets/Ylang-ylang.jpeg",
         desc: "Nos huiles essentielles d'ylang-ylang captivent par leur fragrance florale exquise. Distillées à partir des fleurs de notre plantation, ces huiles captent l'essence même de l'ylang-ylang, offrant une expérience olfactive sophistiquée. Utilisez-les pour ajouter une touche d'élégance à vos créations cosmétiques, parfums ou soins du corps."
     },
     {
         id:"3",
         nomProduit: "Huile Essentielle de Calophylum ",
         itemProduit: "La Puissance Régénérante",
-        photo: "/src/assets/calophylum.jpg",
+        photo: "./src/assets/calophylum.jpg",
         desc: "L'huile essentielle de calophylum, extraite des graines de l'arbre calophylum, est un trésor naturel aux propriétés régénérantes. Réputée pour ses bienfaits pour la peau, cette huile apporte une touche de soin naturel. Intégrez-la dans vos produits de soin pour une expérience beauté authentique et bienfaisante."
     },
     {
         id:"4",
         nomProduit: "Huile de Coco ",
         itemProduit: "La Douceur Tropicale",
-        photo: "/src/assets/coco.jpg",
+        photo: "./src/assets/coco.jpg",
         desc: "Notre huile de coco est une symphonie de douceur tropicale. Pressée à partir de noix de coco fraîches, elle est polyvalente et sert d'ingrédient nutritif pour la cuisine, les soins capillaires et la peau. Découvrez la pureté de l'huile de coco VanillaVoyage pour ajouter une touche tropicale à vos créations."
     }
 ]
